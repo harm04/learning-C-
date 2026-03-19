a8dcffe7b6a065727634e56da91ec7ddd4b6ccbd
sanjamrnd <sanjam.rndtechnosoft@gmail.com>
2026-03-19 08:58:33 +0530

    loops

---
+++ loops.cpp
@@ -0,0 +1,22 @@
+#include    <iostream>
+using namespace std;
+
+int main(){
+    cout << "for loop: "<<endl;
+    for (int i = 0; i < 10;i++){
+    cout << i <<"hello dev" << endl;
+}
+
+int j = 0;
+cout << "while loop: "<<endl;
+while(j<10){
+    cout<< j << "hello dev" << endl;
+    j++;
+}
+int k = 0;
+cout << "do while loop: "<<endl;
+do{
+    cout << k<< "hello dev" << endl;
+    k++;
+}while(k<10);
+}
