066f64951c6ba056596288b234e8488452503706
sanjamrnd <sanjam.rndtechnosoft@gmail.com>
2026-03-19 08:28:26 +0530

    if_else

--- input_output.cpp
+++ input_output.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 
 int main(){
-  int x;
-  cout<<"Enter a value for x: ";
-  cin>>x;
-  cout<<"x has the value "<<x;
+  // int x;
+  // cout<<"Enter a value for x: ";
+  // cin>>x;
+  // cout<<"x has the value "<<x;
+
+  //get string as input and print it
+  string str;
+  cout<<"Enter a value for str: ";
+  getline(cin, str);
+  cout << str;
   return 0; 
 }
